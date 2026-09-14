50e3f0e327da8c1e6d016ada56dd137f4aa307bf
Ohene <acme222001@gmail.com>
2026-09-14 10:44:46 -0800

    Fix users authentication columns migration

--- database/migrations/2026_09_14_183448_add_auth_columns_to_users_table.php
+++ database/migrations/2026_09_14_183448_add_auth_columns_to_users_table.php
@@ -6,23 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('name')->after('id');
+            $table->string('email')->unique()->after('name');
+            $table->timestamp('email_verified_at')->nullable()->after('email');
+            $table->string('password')->after('email_verified_at');
+            $table->rememberToken()->after('password');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn([
+                'name',
+                'email',
+                'email_verified_at',
+                'password',
+                'remember_token',
+            ]);
         });
     }
 };
